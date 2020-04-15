35765a1c78df9f4fdb5a994a421deb1d2e80c3b5
chauncey1010 <developerworld@icloud.com>
2020-04-16 03:20:38 +0800

    第一周

---
+++ components/category-grid/index.js
@@ -0,0 +1,22 @@
+// components/category-grid/index.js
+import array from "../../miniprogram_npm/lin-ui/common/async-validator/validator/array";
+
+Component ({
+
+  properties: {
+    grid: Array
+  },
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  methods: {
+
+  }
+
+ 
+})
---
+++ utils/http.js
@@ -0,0 +1,29 @@
+import {config} from "../config/config";
+import {promisic} from "./util";
+
+class Http {
+    static async request({
+                    url,
+                    data,
+                    method='GET'
+                }) {
+      const res = await promisic(wx.request)({
+            url: `${config.apiBaseUrl}${url}`,
+            data,
+            method,
+            header: {
+                appkey: config.appkey
+            }
+        })
+        return res.data
+    }
+}
+
+// promisic(wx.request)({
+//     url: '',
+//     data: data,
+// })
+
+export {
+    Http
+}
