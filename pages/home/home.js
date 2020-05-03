9969d4e6f1fc7c9bc558beabdaa884a051ebdab0
chauncey1010 <developerworld@icloud.com>
2020-05-02 23:30:57 -0400

    第3周2-1
    关于Activity报错修改

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,7 +3,7 @@
 import {Theme} from "../../model/theme";
 import {Banner} from "../../model/banner";
 import {Category} from "../../model/category";
-import {Activity} from "../../model/Activity";
+import {Activity} from "../../model/activity";
 
 Page({
 
